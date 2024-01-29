02d0aac2a86d9203348086c3866a5fb10b04a7b4
Тэнгис Ариунболд <HP236500@G302-CC0145.local>
2024-01-29 19:30:14 +0800

    first

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,80 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Button, Image, Platform, StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export default function TabOneScreen() {
+  const [image, setImage] = useState<
+    ImagePicker.ImagePickerAsset | undefined
+  >();
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
+  if (!status || !status.granted) {
+    return (
+      <View style={styles.container}>
+        <Text>Give camera permission</Text>
+        <Button title="Enable camera" onPress={requestPermission} />
+      </View>
+    );
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    }
+  };
+
+  const captureImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    }
+  };
+  const recognizeImage = async () => {
+    if (image) {
+      const data = new FormData();
+      // @ts-ignore
+      data.append("image", {
+        name: "image",
+        type: image.type,
+        uri: image.uri,
+      });
+
+      fetch(`https://imaaager.onrender.com/api/upload`, {
+        method: "POST",
+        body: data,
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log("response", response);
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Choose image from gallery" onPress={pickImage} />
+      <Button title="Capture image" onPress={captureImage} />
+      {image && (
+        <View>
+          <Image
+            source={{ uri: image.uri }}
+            style={{ width: 200, height: 200 }}
+          />
+          <Button title="generate tags" onPress={recognizeImage} />
+        </View>
+      )}
     </View>
   );
 }
@@ -16,16 +82,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,107 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default function TabTwoScreen() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [image, setImage] = useState("");
+  const [camera, setCamera] = useState<any>(null);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>Camera permission</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  const handleCapture = async () => {
+    if (camera) {
+      const result = await camera.takePictureAsync();
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Camera
+        style={{ flex: 1, width: "100%" }}
+        type={type}
+        ref={(ref) => setCamera(ref)}
+      >
+        {image && (
+          <Image
+            source={{ uri: image }}
+            width={100}
+            height={100}
+            style={{ borderWidth: 1, borderColor: "red" }}
+          />
+        )}
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <TouchableOpacity
+            onPress={handleCapture}
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              marginBottom: 30,
+              marginLeft: 95,
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "grey",
+                borderRadius: 50,
+                width: 70,
+                height: 70,
+                borderWidth: 2,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={toggleCameraType}
+            style={{
+              justifyContent: "flex-end",
+              marginBottom: 20,
+              marginRight: 20,
+            }}
+          >
+            <View
+              style={{
+                borderRadius: 50,
+                width: 60,
+                height: 60,
+                alignItems: "center",
+                justifyContent: "center",
+                borderWidth: 2,
+                borderColor: "red",
+              }}
+            >
+              <Text style={{ color: "red" }}>Flip</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -16,16 +109,11 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontWeight: "bold",
   },
 });
